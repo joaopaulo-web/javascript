97b8887ee3be6267835b1810ed9d153deb7e2f86
JoaoPaulo-Web <joaopaulocoimbra2013@gmail.com>
2023-01-18 15:41:40 -0300

    exercicios 11 e 13 +  imagens para exercicio 13

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 22
+if (idade < 16){
+    console.log('Não vota')
+}else{
+    if(idade < 18){
+        console.log('Voto opicional.')
+    }else{
+        console.log('Voto obrigatório')
+    }
+}
---
+++ exercicios-java/java.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'img/manha.png'
+        document.body.style.background = '#9abde6'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#e5915d'
+    } else{
+        img.src = 'img/noite.png'
+        document.body.style.background = '#37270e'
+    }
+}
