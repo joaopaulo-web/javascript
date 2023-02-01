16f3478f0398e7f1e7f33e000958ea99477604bd
JoaoPaulo-Web <joaopaulocoimbra2013@gmail.com>
2023-02-01 14:52:09 -0300

    Inicio de Mini Projetos.

---
+++ por-fora/mini2.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Digite um Número: ')
+let num1Convertido = parseInt(num1)
+let num2 = prompt('Digite um Segundo Número: ')
+let num2Convertido = parseInt(num2)
+
+
+if (num1Convertido > num2Convertido){
+    alert(`O maior número digitado foi ${num1Convertido}`)
+}else{
+    alert(`O maior número digitado foi ${num2Convertido}`)
+}
